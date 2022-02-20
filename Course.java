a1d2e4dcae086e82d53fa96ede080bf22d85af92
Shang-Yu Chan <87119388+5432roy@users.noreply.github.com>
2022-02-20 00:04:33 -0800

    fix the wrong file name for course

---
+++ Course.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+
+public class Course implements Comparable {
+
+    private String abbrevName;
+    private String fullName;
+    private int number;
+    private ArrayList<ArrayList<Course>> preReqs;
+
+    public Course(String abbrevName, String fullName, int number, ArrayList<ArrayList<Course>> preReqs){
+        this.abbrevName = abbrevName;
+        this.fullName = fullName;
+        this.number = number;
+        this.preReqs = preReqs;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getAbbrevName(){
+        return abbrevName;
+    }
+    
+    public String getFullName(){
+        return fullName;
+    }
+    
+    public ArrayList<ArrayList<Course>> getPreReqs(){
+        return preReqs;
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        if(this.number > (int)(obj)){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+}
